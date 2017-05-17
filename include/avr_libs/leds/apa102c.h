f3a2e65582a0bb660d6fe05577ad3c5a553464ca
djdt <djdt@djdt.djdt>
2017-05-17 17:20:32 +1000

    made apa102c object to store pins/port

--- include/avr_libs/leds/apa102c.h
+++ include/avr_libs/leds/apa102c.h
@@ -5,13 +5,17 @@
 
 #include "avr_libs/leds/rgb_led.h"
 
-#define APA102C_PORT PORTA
-#define APA102C_PIN  PINA
-#define APA102C_DDR  DDRA
-#define APA102C_PIN_CO 0
-#define APA102C_PIN_DO 0
+typedef struct {
+	uint8_t* ddr;
+	uint8_t* port;
+	uint8_t  clock_pin;
+	uint8_t  data_pin;
+} apa102c_t;
+
+// Setup for pins.
+void rgb_led_init(apa102c_t* apa102c);
 
 // Only considers the 5 lsb of brightness.
-void rgb_led_set_leds(rgb_led_t* leds, uint8_t num_leds);
+void rgb_led_set_leds(apa102c_t* apa102c, rgb_led_t* leds, uint8_t num_leds);
 
 #endif /* ifndef _AVR_LIBS_LED_APA102C_H_ */
--- leds/apa102c/apa102c.c
+++ leds/apa102c/apa102c.c
@@ -4,67 +4,50 @@
 
 #define APA102C_DELAY 0.5
 
-void rgb_led_init()
+void rgb_led_init(apa102c_t* apa102c)
 {
 	// Set high
-	APA102C_PORT |= (1<<APA102C_PIN_CO)|(1<<APA102C_PIN_DO);
+	*(apa102c->port) |= (1<<apa102c->clock_pin)|(1<<apa102c->data_pin);
 	// Set as outputs
-	APA102C_DDR  |= (1<<APA102C_PIN_CO)|(1<<APA102C_PIN_DO);
+	*(apa102c->ddr)  |= (1<<apa102c->clock_pin)|(1<<apa102c->data_pin);
 }
 
-void rgb_led_send_byte(uint8_t byte)
+void rgb_led_send_byte(apa102c_t* apa102c, uint8_t byte)
 {
 	uint8_t i;
 	for (i = 0; i < 8; ++i) {
-		APA102C_PORT &= ~(1<<APA102C_PIN_CO); // Pull low
+		*(apa102c->port) &= ~(1<<apa102c->clock_pin); // Pull low
 
 		if (byte & 0x80) // msb
-			APA102C_PORT |=  (1<<APA102C_PIN_DO);
+			*(apa102c->port) |=  (1<<apa102c->data_pin);
 		else
-			APA102C_PORT &= ~(1<<APA102C_PIN_DO);
+			*(apa102c->port) &= ~(1<<apa102c->data_pin);
 
 		_delay_us(APA102C_DELAY); // Clock low
 
-		APA102C_PORT |=  (1<<APA102C_PIN_CO); // Pull high
+		*(apa102c->port) |=  (1<<apa102c->clock_pin); // Pull high
 		_delay_us(APA102C_DELAY); // Clock high
 
 		byte = byte << 1;
 	}
 }
 
-void rgb_send_data(uint8_t* data, uint8_t data_len)
+void rgb_led_set_leds(apa102c_t* apa102c, rgb_led_t* leds, uint8_t num_leds)
 {
 	uint8_t i;
 	// Start frame
 	for (i = 0; i < 4; ++i) {
-		rgb_led_send_byte(0x00);
-	}
-	// Data
-	for (i = 0; i < data_len; ++i) {
-		rgb_led_send_byte(*(data)++);
-	}
-	// End frame
-	for (i = 0; i < 4; ++i) {
-		rgb_led_send_byte(0xff);
-	}
-}
-
-void rgb_led_set_leds(rgb_led_t* leds, uint8_t num_leds)
-{
-	uint8_t i;
-	// Start frame
-	for (i = 0; i < 4; ++i) {
-		rgb_led_send_byte(0x00);
+		rgb_led_send_byte(apa102c, 0x00);
 	}
 	// Data
 	for (i = 0; i < num_leds; ++i) {
-		rgb_led_send_byte(0xe0 | (*leds).brightness);
-		rgb_led_send_byte((*leds).red);
-		rgb_led_send_byte((*leds).green);
-		rgb_led_send_byte((*leds).blue);
+		rgb_led_send_byte(apa102c, 0xe0 | (*leds).brightness);
+		rgb_led_send_byte(apa102c, (*leds).red);
+		rgb_led_send_byte(apa102c, (*leds).green);
+		rgb_led_send_byte(apa102c, (*leds).blue);
 	}
 	// End frame
 	for (i = 0; i < 4; ++i) {
-		rgb_led_send_byte(0xff);
+		rgb_led_send_byte(apa102c, 0xff);
 	}
 }
