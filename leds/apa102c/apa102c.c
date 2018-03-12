375ba0de42fa1d38f7364a2d6d2f78a926f02fa2
djdt <10266332+djdt@users.noreply.github.com>
2018-03-12 21:12:46 +1100

    fixed led order

--- leds/apa102c/apa102c.c
+++ leds/apa102c/apa102c.c
@@ -7,7 +7,7 @@ void rgb_led_init(void)
 	// Set high
 	APA102C_PORT |= (1<<APA102C_PIN_CLK)|(1<<APA102C_PIN_DATA);
 	// Set as outputs
-	APA102C_DDR	 |= (1<<APA102C_PIN_CLK)|(1<<APA102C_PIN_DATA);
+	APA102C_DDR  |= (1<<APA102C_PIN_CLK)|(1<<APA102C_PIN_DATA);
 }
 
 void rgb_led_send_byte(uint8_t byte)
@@ -39,9 +39,9 @@ void rgb_led_set_leds(rgb_led_t* leds, uint8_t num_leds)
 	// Data
 	for (i = 0; i < num_leds; ++i) {
 		rgb_led_send_byte(0xe0 | (*leds).brightness);
-		rgb_led_send_byte((*leds).color.r);
-		rgb_led_send_byte((*leds).color.g);
 		rgb_led_send_byte((*leds).color.b);
+		rgb_led_send_byte((*leds).color.g);
+		rgb_led_send_byte((*leds).color.r);
 		++leds;
 	}
 	// End frame (unneeded?)
